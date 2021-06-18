e603ffd7c211170df0a5a599ac7c91043f134792
Phil Varner <philvarner@gmail.com>
2021-06-18 17:33:56 -0400

    feat: datetime query parameter validation

--- stac_api_validator/validate_all.py
+++ stac_api_validator/validate_all.py
@@ -1,29 +1,35 @@
 from validations import validate_api
+import traceback
 
-roots = [ 
+
+roots = [
     'https://earth-search.aws.element84.com/v0',
     'https://planetarycomputer.microsoft.com/api/stac/v1/',
     'https://eod-catalog-svc-prod.astraea.earth/',
     'https://services.sentinel-hub.com/api/v1/catalog/',
-    'https://api.radiant.earth/mlhub/v1/',
     'https://earthengine.openeo.org/v1.0',
     'https://franklin.nasa-hsi.azavea.com/',
     'https://tamn.snapplanet.io/',
-    'https://data.geo.admin.ch/api/stac/v0.9/',
     'https://cmr.earthdata.nasa.gov/stac/LARC_ASDC',
-    'https://landsatlook.usgs.gov/sat-api/stac'
- ]
+]
 
 for root in roots:
     try:
         print(f"Validating {root} ...", flush=True)
 
-        (errors, warnings) = validate_api(root)
+        (warnings, errors) = validate_api(root)
 
-        print(f"warnings: {warnings}")
-        print(f"errors: {errors}")
+        print(f"warnings:")
+        for warning in warnings:
+            print(warning)
+        print(f"errors:")
+        for error in errors:
+            print(error)
 
         if not warnings and not errors:
-            print("valid!")
+            print("Valid! 🎉🎉🎉")
+
+        print("---------------------------------------------------")
     except Exception as e:
         print(f"fail.\nError {root}: {type(e)} {str(e)}")
+        traceback.print_exc()
--- stac_api_validator/validations.py
+++ stac_api_validator/validations.py
@@ -17,6 +17,62 @@ oaf_cc_regex = re.compile(
 search_cc_regex = re.compile('https://api\.stacspec\.org/.+/item-search')
 
 openapi_media_type = "application/vnd.oai.openapi+json;version=3.0"
+geojson_mt = 'application/geo+json'
+geojson_charset_mt = 'application/geo+json; charset=utf-8'
+
+valid_datetimes = [
+        "1985-04-12T23:20:50.52Z",
+        "1985-04-12T23:20:50,52Z",
+        "1996-12-19T16:39:57-00:00",
+        "1996-12-19T16:39:57+00:00",
+        "1996-12-19T16:39:57-08:00",
+        "1996-12-19T16:39:57+08:00",
+        "../1985-04-12T23:20:50.52Z",
+        "1985-04-12T23:20:50.52Z/..",
+        "/1985-04-12T23:20:50.52Z",
+        "1985-04-12T23:20:50.52Z/",
+        "1985-04-12T23:20:50.52Z/1986-04-12T23:20:50.52Z",
+        "1985-04-12T23:20:50.52+01:00/1986-04-12T23:20:50.52Z+01:00",
+        "1985-04-12T23:20:50.52-01:00/1986-04-12T23:20:50.52Z-01:00",
+
+        "1937-01-01T12:00:27.87+01:00",
+        "1985-04-12T23:20:50.52Z",
+        "1937-01-01T12:00:27.8710+01:00",
+        "1937-01-01T12:00:27.8+01:00",
+        "1937-01-01T12:00:27.8Z",
+
+        "2020-07-23T00:00:00.000+03:00",
+        "2020-07-23T00:00:00+03:00",
+        "1985-04-12t23:20:50.000z",
+
+        "2020-07-23T00:00:00Z",
+        "2020-07-23T00:00:00.0Z",
+        "2020-07-23T00:00:00.01Z",
+        "2020-07-23T00:00:00.012Z",
+        "2020-07-23T00:00:00.0123Z",
+        "2020-07-23T00:00:00.01234Z",
+        "2020-07-23T00:00:00.012345Z",
+        "2020-07-23T00:00:00.0123456Z",
+        "2020-07-23T00:00:00.01234567Z",
+        "2020-07-23T00:00:00.012345678Z",
+    ]
+
+invalid_datetimes = [
+        "1985-04-12",  # date only
+        "1937-01-01T12:00:27.87+0100",  # invalid TZ format, no sep :
+        "37-01-01T12:00:27.87Z",  # invalid year, must be 4 digits
+        "1985-12-12T23:20:50.52",  # no TZ
+        "21985-12-12T23:20:50.52Z",  # year must be 4 digits
+        "1985-13-12T23:20:50.52Z",  # month > 12
+        "1985-12-32T23:20:50.52Z",  # day > 31
+        "1985-12-01T25:20:50.52Z",  # hour > 24
+        "1985-12-01T00:60:50.52Z",  # minute > 59
+        "1985-12-01T00:06:61.52Z",  # second > 60
+        "1985-04-12T23:20:50.Z",  # fractional sec . but no frac secs
+        "1985-04-12T23:20:50,Z",  # fractional sec , but no frac secs
+        "1990-12-31T23:59:61Z",  # second > 60 w/o fractional seconds
+        "1986-04-12T23:20:50.52Z/1985-04-12T23:20:50.52Z",
+    ]
 
 def validate_api(root_uri: str) -> Tuple[List[str], List[str]]:
     logger = logging.getLogger(__name__)
@@ -27,7 +83,7 @@ def validate_api(root_uri: str) -> Tuple[List[str], List[str]]:
     catalog: Client = Client.open(root_uri)
 
     if not catalog.conformance:
-        errors.append("/ : 'conformsTo' field must be defined and non-empty.")
+        errors.append("/ : 'conformsTo' field must be defined and non-empty. This field is required as of 1.0.0.")
 
     if not catalog.links:
         errors.append("/ : 'links' field must be defined and non-empty.")
@@ -41,7 +97,7 @@ def validate_api(root_uri: str) -> Tuple[List[str], List[str]]:
 
     # fail fast if there are errors with conformance or links so far
     if errors:
-        return (warnings, errors)        
+        return (warnings, errors)
 
     # todo: option to force validation and warn about non-existence of CC
     # todo: allow deprecated conformance classes, and warn
@@ -54,7 +110,7 @@ def validate_api(root_uri: str) -> Tuple[List[str], List[str]]:
     else:
         errors.append(
             "/ : 'conformsTo' must contain STAC API - Core conformance class.")
-    
+
     if any(oaf_cc_regex.match(x) for x in catalog.conformance):
         print("STAC API - Features conformance class found.")
         validate_oaf(catalog, warnings, errors)
@@ -69,8 +125,9 @@ def validate_api(root_uri: str) -> Tuple[List[str], List[str]]:
 
     return (warnings, errors)
 
+
 def validate_core(catalog: Client, warnings: List[str],
-                  errors: List[str] = []) -> int:
+                  errors: List[str]) -> int:
 
     # Link rel=root
     root: Link = catalog.get_single_link('root')
@@ -90,34 +147,34 @@ def validate_core(catalog: Client, warnings: List[str],
 
     # Link rel=service-desc
     service_desc: Link = catalog.get_single_link('service-desc')
-    if service_desc is not None:
+    if not service_desc:
+        warnings.append("/ : Link[rel=service-desc] should exist")
+    else:
         if service_desc.media_type != openapi_media_type:
             errors.append(
                 f"/ : Link[rel=service-desc] should have media_type '{openapi_media_type}'', actually '{service_desc.media_type}'")
-    else:
-        warnings.append("/ : Link[rel=service-desc] should exist")
 
-    r_service_desc = requests.get(service_desc.get_absolute_href())
+        r_service_desc = requests.get(service_desc.get_absolute_href())
 
-    errors += validate(
-        "/ : Link[service-desc] must return 200",
-        lambda: r_service_desc.status_code == 200)
+        errors += validate(
+            "/ : Link[service-desc] must return 200",
+            lambda: r_service_desc.status_code == 200)
 
-    if (ct := r_service_desc.headers.get('content-type')) == openapi_media_type:
-        pass
-    else:
-        errors.append(
-            f"service-desc ({service_desc.get_absolute_href()}): should have content-type header '{openapi_media_type}'', actually '{ct}'")
+        if (ct := r_service_desc.headers.get('content-type')) == openapi_media_type:
+            pass
+        else:
+            errors.append(
+                f"service-desc ({service_desc.get_absolute_href()}): should have content-type header '{openapi_media_type}'', actually '{ct}'")
 
-    try:
-        r_service_desc.json()
-    except json.decoder.JSONDecodeError as e:
-        errors.append(
-            f"service-desc ({service_desc.get_absolute_href()}): should return JSON, instead got non-JSON text")
+        try:
+            r_service_desc.json()
+        except json.decoder.JSONDecodeError as e:
+            errors.append(
+                f"service-desc ({service_desc.get_absolute_href()}): should return JSON, instead got non-JSON text")
 
     # Link rel=service-doc
     service_doc: Link = catalog.get_single_link('service-doc')
-    if service_doc is None:
+    if not service_doc:
         warnings.append("/ : Link[rel=service-doc] should exist")
     else:
         if service_doc.media_type != "text/html":
@@ -129,11 +186,11 @@ def validate_core(catalog: Client, warnings: List[str],
             "/ : Link[service-doc] must return 200",
             lambda: r_service_doc.status_code == 200)
 
-        if (ct := r_service_doc.headers.get('content-type')) == 'text/html':
+        if (ct := r_service_doc.headers.get('content-type')).startswith('text/html'):
             pass
         else:
             errors.append(
-                f"service-doc ({service_doc.get_absolute_href()}): should have content-type header '{openapi_media_type}'', actually '{ct}'")
+                f"service-doc ({service_doc.get_absolute_href()}): should have content-type header 'text/html', actually '{ct}'")
 
     # try:
     #     r_service_doc.html()
@@ -143,12 +200,12 @@ def validate_core(catalog: Client, warnings: List[str],
 
 
 def validate_oaf(catalog: Client, warnings: List[str],
-                 errors: List[str] = []) -> int:
+                 errors: List[str]) -> int:
 
     conformance = catalog.get_single_link('conformance')
     errors += validate(
         "/ Link[rel=conformance] should href /conformance",
-        lambda: conformance and conformance.target.endswith("/conformance") 
+        lambda: conformance and conformance.target.endswith("/conformance")
     )
 
     r_conformance = requests.get(conformance.get_absolute_href())
@@ -175,7 +232,6 @@ def validate_oaf(catalog: Client, warnings: List[str],
         errors.append(
             f"service-desc ({conformance.get_absolute_href()}): should return JSON, instead got non-JSON text")
 
-
     errors += validate(
         "/ Link[rel=data] should href /collections",
         lambda: catalog.get_single_link('data')
@@ -190,17 +246,18 @@ def validate_oaf(catalog: Client, warnings: List[str],
 
 # todo: child and children?
 
+
 def validate_search(catalog: Client, warnings: List[str],
-                    errors: List[str] = []) -> int:
+                    errors: List[str]) -> int:
 
     search: Link = catalog.get_single_link('search')
-    geojson = 'application/geo+json'
     r = requests.get(search.get_absolute_href())
-    if (ct := r.headers.get('content-type')) == geojson:
+    content_type: str = r.headers.get('content-type')
+    if (content_type == geojson_mt or content_type == geojson_charset_mt):
         pass
     else:
         errors.append(
-            f"Search ({search.get_absolute_href()}): should have content-type header '{geojson}'', actually '{ct}'")
+            f"Search ({search.get_absolute_href()}): should have content-type header '{geojson_mt}'', actually '{content_type}'")
 
     errors += validate(
         f"Search({search.get_absolute_href()}): should return 200",
@@ -216,6 +273,45 @@ def validate_search(catalog: Client, warnings: List[str],
     # todo: run a real query
     mysearch = catalog.search(max_items=1)
     mysearch.items_as_collection
+
+    validate_search_datetime(search.get_absolute_href(), warnings, errors)
+
+
+def validate_search_datetime(
+    search_url: str,
+    warnings: List[str],
+    errors: List[str] = []
+):
+
+    # find an Item and try to use its datetime value in a query
+    r = requests.get(search_url)
+    dt = r.json()["features"][0]["properties"]["datetime"]
+    r = requests.get(search_url, params={"datetime": dt})
+    if r.status_code != 200:
+        errors.append(
+            f"Search with datetime={dt} extracted from an Item returned status code {r.status_code}")
+    elif len(r.json()["features"]) == 0:
+        errors.append(
+            f"Search with datetime={dt} extracted from an Item returned no results.")
+
+    for dt in valid_datetimes:
+        r = requests.get(search_url, params={"datetime": dt})
+        if r.status_code != 200:
+            errors.append(
+                f"Search with datetime={dt} returned status code {r.status_code}")
+            continue
+        try:
+            r.json()
+        except json.decoder.JSONDecodeError:
+            errors.append(
+                f"Search with datetime={dt} returned non-json response")
+    
+    for dt in invalid_datetimes:
+        r = requests.get(search_url, params={"datetime": dt})
+        if r.status_code != 400:
+            errors.append(
+                f"Search with datetime={dt} returned status code {r.status_code} instead of 400")
+            continue
 
 
 def validate(error_str: str, p: Callable[[], bool]) -> List[str]:
